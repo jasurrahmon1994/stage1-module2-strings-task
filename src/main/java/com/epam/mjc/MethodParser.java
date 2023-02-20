6bebf4c1ae29124052726b9b5b5f49d97f83dc6e
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-02-20 14:03:58 +0500

    first try

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,8 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
@@ -19,7 +22,35 @@ public class MethodParser {
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
+
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+//        throw new UnsupportedOperationException("You should implement this method.");
+        List<MethodSignature.Argument> args = new ArrayList<>();
+        int braceIndex = signatureString.indexOf("(");
+        String argStr = signatureString.substring(braceIndex + 1, signatureString.length() - 1);
+        if(argStr.length() > 0){
+            String[] split = argStr.split(", ");
+            for (int i = 0; i < split.length; i++) {
+                String[] s = split[i].split(" ");
+                MethodSignature.Argument argument = new MethodSignature.Argument(s[0], s[1]);
+                args.add(i, argument);
+            }
+        }
+        String methodStr = signatureString.substring(0, braceIndex);
+        String[] s = methodStr.split(" ");
+        String acMod, retType, metName;
+        if(s.length == 2){
+            acMod = null;
+            retType = s[0];
+            metName = s[1];
+        } else {
+            metName = s[2];
+            acMod = s[0];
+            retType = s[1];
+        }
+        MethodSignature ms = new MethodSignature(metName, args);
+        ms.setAccessModifier(acMod);
+        ms.setReturnType(retType);
+        return ms;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,7 +1,9 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringSplitter {
 
@@ -12,7 +14,20 @@ public class StringSplitter {
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
+
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+//        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> res = new ArrayList<>();
+        String temp = "";
+        for (int i = 0; i < source.length(); i++) {
+            if(delimiters.contains(source.charAt(i) + "")){
+                res.add(temp);
+                temp = "";
+            } else {
+                temp += source.charAt(i);
+            }
+        }
+        res.add(temp);
+        return res.stream().filter(s -> s.length() > 0).collect(Collectors.toList());
     }
 }
